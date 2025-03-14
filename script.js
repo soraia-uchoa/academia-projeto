aa89b6eed00f606ab159d7d97ac63f216b082594
soraia <soraiavercosa3@icloud.com>
2025-03-14 08:53:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function usuario() {
+    alert('funcionou')
+    
+    if (planoMensal === 'Plano mensal:100 R$')
+        alert('seu plano mensal:100 R$ ', ' + nome + ', ' + peso + ', ' + alutura + ')
+}
