da541055d9a955917b354c44635b310eff087789
soraia <soraiavercosa3@icloud.com>
2025-03-18 08:53:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function usuario(user) {
-    const {name, button1} = user;
-
-    if (button1 === 'plano mensal: 100 R$') {
-        alert('você escolheu o plano mensal de 100 R$, ' + name + '!');
-    }
+function usuario1() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu o plano de 100 R$, ${nome}`)
 }
+
+function usuario2() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu o plano 3x de 95 R$, ${nome}`)
+}
+
+function usuario3() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu o plano 6x de 90 R$, ${nome}`)
+}
+
+function usuario4() {
+    const nome = document.getElementById('fname').value
+    alert(`você escolheu diaria de 30 R$, ${nome}`)
+}
+
+
